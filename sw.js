8ebb55a66ee5906f058e348ed12efc9739ce3e52
buddycampingstore <onestopservicealc@gmail.com>
2026-07-27 20:40:24 +0700

    แก้ icon

---
+++ sw.js
@@ -0,0 +1,22 @@
+const CACHE = 'alc-hub-v2';
+const SHELL = ['./', './manifest.json', './icons/logo.png', './icons/icon-192.png', './icons/icon-512.png', './icons/icon-maskable-512.png'];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(SHELL)).catch(() => {}).then(() => self.skipWaiting()));
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(caches.keys().then(ks => Promise.all(ks.filter(k => k !== CACHE).map(k => caches.delete(k)))).then(() => self.clients.claim()));
+});
+
+self.addEventListener('fetch', e => {
+  const req = e.request;
+  if (req.method !== 'GET' || new URL(req.url).origin !== location.origin) return;
+  e.respondWith(
+    fetch(req).then(res => {
+      const copy = res.clone();
+      caches.open(CACHE).then(c => c.put(req, copy)).catch(() => {});
+      return res;
+    }).catch(() => caches.match(req).then(r => r || caches.match('./')))
+  );
+});
